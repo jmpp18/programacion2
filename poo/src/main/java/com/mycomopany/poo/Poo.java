9bf4a4e835a0110345051de02f0e6c9a42f21d94
jmpp <jmperezp@ufpso.edu.co>
2024-03-01 11:31:31 -0500

    creacion de clase interna

--- poo/src/main/java/com/mycomopany/poo/Poo.java
+++ poo/src/main/java/com/mycomopany/poo/Poo.java
@@ -1,10 +1,35 @@
-
 package com.mycomopany.poo;
-
 
 public class Poo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        character homero = new character("homero", "inspector de seguridad",33,"que elegancia la de francia");
+        homero.greeting();
+       character bart = new character("bartolomeo","vago",10,"ayyy caramba!!!");
+       bart.greeting();
+        
+    }
+
+    static class character {
+
+        String name;
+        String occupation;
+        int age;
+        String phrase;
+
+        public character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+        
+        
+        
+        void greeting() {
+           
+            System.out.println(this.phrase);
+             System.out.println(this.age);
+        }
     }
 }
